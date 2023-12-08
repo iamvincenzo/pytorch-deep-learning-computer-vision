46a6650aa56f62ba863595ad5ad1392759319c12
iamvincenzo <vincenzo.fraello@outlook.it>
2023-12-08 01:57:40 +0100

    update solver.py

--- binary-classification/solver.py
+++ binary-classification/solver.py
@@ -1,7 +1,14 @@
 import torch
 import numpy as np
 from tqdm import tqdm
+from torchmetrics import ROC
+from sklearn.metrics import auc
+from timeit import default_timer as timer
+from torchmetrics import PrecisionRecallCurve
+from sklearn.metrics import cohen_kappa_score
 
+from plotting_utils import plot_roc
+from plotting_utils import plot_prc
 from early_stopping import EarlyStopping
 from plotting_utils import plot_confusion_matrix
 
@@ -31,6 +38,14 @@ class Solver(object):
         self.criterion = criterion
         self.patience = patience
 
+        self.model_reslts = {
+            "test_name" : "",
+            "model_name": self.model.__class__.__name__,
+            "train_time": 0.0,
+            "model_loss": 0.0,
+            "model_acc": 0.0
+        }
+
     def train_net(self):
         """
         Trains the neural network using the specified DataLoader for training data.
@@ -50,14 +65,16 @@ class Solver(object):
 
         # initialize the early_stopping object
         early_stopping = EarlyStopping(patience=self.patience, verbose=True)
+
+        train_time_start = timer()
         
         self.model.train()
 
         for epoch in range(self.epochs):
             print(f"\nTraining iteration | Epoch[{epoch + 1}/{self.epochs}]")
 
-            predictions = torch.tensor([])
-            targets = torch.tensor([])
+            predictions = torch.tensor([], device=self.device)
+            targets = torch.tensor([], device=self.device)
 
             # use tqdm for a progress bar during training
             loop = tqdm(iterable=enumerate(self.train_loader),
@@ -134,6 +151,10 @@ class Solver(object):
         
         print("\nTraining model Done...\n")
 
+        train_time_end = timer()
+
+        self.model_reslts["train_time"] = train_time_end - train_time_start
+
         # write all remaining data in the buffer
         self.writer.flush()
         # free up system resources used by the writer
@@ -150,8 +171,8 @@ class Solver(object):
         """
         print(f"\nStarting validation...\n")
 
-        predictions = torch.tensor([])
-        targets = torch.tensor([])
+        predictions = torch.tensor([], device=self.device)
+        targets = torch.tensor([], device=self.device)
 
         self.model.eval()
 
@@ -193,6 +214,68 @@ class Solver(object):
         # set the model back to training mode
         self.model.train()
 
+    def eval_model(self):
+        """
+        Evaluates a given model on a given dataset.
+
+        Args:
+            model (torch.nn.Module): A PyTorch model capable of making predictions on data_loader.
+            data_loader (torch.utils.data.DataLoader): The target dataset to predict on.
+            loss_fn (torch.nn.Module): The loss function of model.
+            accuracy_fn: An accuracy function to compare the models predictions to the truth labels.
+            device (str, optional): Target device to compute on. Defaults to device.
+
+        Returns:
+            (dict): Results of model making predictions on data_loader.
+        """    
+        print(f"\nStarting final evaluation...\n")
+
+        predictions = torch.tensor([], device=self.device)
+        targets = torch.tensor([], device=self.device)
+        eval_losses = [] 
+
+        self.model.eval()
+
+        # use tqdm for a progress bar during validation
+        with torch.inference_mode():
+            loop = tqdm(iterable=enumerate(self.test_loader), 
+                        total=len(self.test_loader), 
+                        leave=True)
+
+            for _, (x_valid, y_valid) in loop:
+                # move data and labels to the specified device
+                x_valid = x_valid.to(self.device)
+                y_valid = y_valid.unsqueeze(1).to(self.device)
+
+                # forward pass: compute predicted outputs by passing inputs to the model
+                logits = self.model(x_valid)
+
+                # calculate the loss
+                loss = self.criterion(logits, y_valid)
+
+                # record validation loss
+                eval_losses.append(loss.item())
+
+                # since we are using BCEWithLogitsLoss
+                # logits --> probabilities --> labels
+                probs = torch.sigmoid(logits)
+                y_pred = torch.round(probs)
+
+                # record predictions and true labels
+                predictions = torch.cat([predictions, y_pred], dim=0)
+                targets = torch.cat([targets, y_valid], dim=0)
+
+            eval_loss = np.average(eval_losses)
+
+            targets = targets.squeeze()
+            predictions = predictions.squeeze()
+            accuracy = torch.sum(predictions == targets).item() / targets.size(0)
+
+            self.model_reslts["model_acc"] = accuracy
+            self.model_reslts["model_loss"] = eval_loss
+
+            return self.model_reslts
+
     def compute_metrics(self, epoch, batch_idx, predictions, targets, train):
         # compute accuracy
         # accuracy = torch.sum(predictions == targets).item() / (targets.size(0) * targets.size(1))
@@ -207,18 +290,28 @@ class Solver(object):
         precision = true_positives / (true_positives + false_positives + 1e-20)
         recall = true_positives / (true_positives + false_negatives + 1e-20)
         f1_score = 2 * (precision * recall) / (precision + recall + 1e-20)
+
+        cohen_kappa = cohen_kappa_score(y1=targets.detach().cpu().numpy(), 
+                                        y2=predictions.detach().cpu().numpy(), 
+                                        weights='quadratic')
         
         # create the confusion matrix
         confusion_matrix = torch.tensor([[true_negatives, false_positives], 
                                          [false_negatives, true_positives]])
+        
+        # # normalize the confusion matrix
+        # row_sums = confusion_matrix.sum(dim=1, keepdim=True)
+        # normalized_confusion_matrix = confusion_matrix / row_sums
 
 
         # print accuracy based on training or validation
         metric_type = "train" if train else "valid"
         print(f"\n{metric_type} accuracy: {accuracy:.3f} | "
-              f"precision: {precision:.3f} | recall: {recall:.3f} | f1_score: {f1_score:.3f} ")
+              f"precision: {precision:.3f} | recall: {recall:.3f} | "
+              f"f1_score: {f1_score:.3f} | cohen-kappa-score: {cohen_kappa:.3f}")
         
         print(f"\nConfusion_matrix: \n{confusion_matrix}")
+        # print(f"\nNormalized Confusion_matrix: \n{normalized_confusion_matrix}")
         
         self.writer.add_scalar(f"{metric_type}-accuracy", accuracy, epoch * len(self.train_loader) + batch_idx)
         self.writer.add_scalar(f"{metric_type}-precision", precision, epoch * len(self.train_loader) + batch_idx)
@@ -227,4 +320,17 @@ class Solver(object):
 
         fig = plot_confusion_matrix(tn=true_negatives, fp=false_positives, 
                                     fn=false_negatives, tp=true_positives)
-        self.writer.add_figure(f"{metric_type}-confusion_matrix", fig, global_step=epoch * len(self.train_loader) + batch_idx)        
+        self.writer.add_figure(f"{metric_type}-confusion_matrix", fig, global_step=epoch * len(self.train_loader) + batch_idx)   
+
+        roc = ROC(task="binary")
+        fpr, tpr, thresholds = roc(predictions.detach().cpu(), 
+                                   targets.type(torch.LongTensor).detach().cpu())
+        roc_auc = auc(fpr, tpr)
+        fig = plot_roc(fpr=fpr, tpr=tpr, auc=roc_auc)
+        self.writer.add_figure(f"{metric_type}-ROC", fig, global_step=epoch * len(self.train_loader) + batch_idx)
+
+        pr_curve = PrecisionRecallCurve(task="binary", thresholds=2)
+        pr, rc, thresholds = pr_curve(predictions.detach().cpu(), 
+                                      targets.type(torch.LongTensor).detach().cpu())
+        fig = plot_prc(precision=pr, recall=rc)
+        self.writer.add_figure(f"{metric_type}-PRC", fig, global_step=epoch * len(self.train_loader) + batch_idx)
