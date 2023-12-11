23f7614eb1182cb1aaa954d264e28ef399bca254
iamvincenzo <vincenzo.fraello@outlook.it>
2023-12-11 16:34:55 +0100

    update hyperparams_optimization.py

--- binary-classification/hyperparams_optimization.py
+++ binary-classification/hyperparams_optimization.py
@@ -1,3 +1,6 @@
+"""
+Credits: https://github.com/optuna/optuna-examples/tree/main/pytorch
+"""
 import os
 import torch
 import optuna
@@ -7,7 +10,7 @@ import pandas as pd
 import torch.nn as nn
 from pathlib import Path
 import torch.optim as optim
-from torchvision import  models
+from torchvision import models
 from torchvision import transforms
 from optuna.trial import TrialState
 from torch.utils.data import DataLoader
@@ -15,7 +18,6 @@ from torchvision.models import ResNet18_Weights
 from sklearn.model_selection import train_test_split
 
 from custom_dataset import CleanDirtyRoadDataset
-
 
 # reproducibility
 SEED = 42
@@ -26,7 +28,7 @@ torch.cuda.manual_seed(SEED)
 # torch.backends.cudnn.benchmark = False
 # torch.backends.cudnn.deterministic = True
 
-# params
+# parameters
 RESIZE = 224
 PATIENCE = 5
 NUM_CLASSES = 1
@@ -43,15 +45,20 @@ base_img_pth = Path("./data/Images")
 metadata_path = Path("./data/metadata.csv")
 
 
-def finetuning():    
+def finetuning():
+    """
+    Create a ResNet18 model with pre-trained weights and customize the final layer for fine-tuning.
+
+    Returns:
+        torch.nn.Module: ResNet18 model with a modified final layer.
+    """
     model = models.resnet18(weights=ResNet18_Weights.DEFAULT)
-    
-    # freeze all params
+
+    # freeze all parameters
     for param in model.parameters():
         param.requires_grad = False
-    
+
     # add a new final layer
-    # number of input features of last layer
     nr_filters = model.fc.in_features
     model.fc = nn.Linear(nr_filters, NUM_CLASSES)
 
@@ -59,15 +66,31 @@ def finetuning():
 
 
 def define_model(trial):
+    """
+    Define a model for Optuna hyperparameter optimization.
+
+    Args:
+        trial (optuna.Trial): The trial object for hyperparameter optimization.
+
+    Returns:
+        torch.nn.Module: A PyTorch model with hyperparameter-specific architecture.
+    """
     pass
 
 
 def get_loaders():
+    """
+    Prepare DataLoader instances for training and testing.
+
+    Returns:
+        DataLoader: Training DataLoader.
+        DataLoader: Testing DataLoader.
+    """
     df = pd.read_csv(filepath_or_buffer=metadata_path)
 
     X_train, X_test, y_train, y_test = train_test_split(df["filename"], df["label"],
                                                         test_size=0.2, random_state=SEED, shuffle=True)
-    
+
     # define data transformations for training and testing
     if APPLY_TRANSF:
         train_transform = transforms.Compose([transforms.Resize(size=(RESIZE, RESIZE)),
@@ -75,7 +98,7 @@ def get_loaders():
                                               transforms.RandomVerticalFlip(p=0.5),
                                               transforms.RandomRotation(degrees=15),
                                               transforms.ToTensor(),
-                                              transforms.Normalize([0.485, 0.456, 0.406], 
+                                              transforms.Normalize([0.485, 0.456, 0.406],
                                                                    [0.229, 0.224, 0.225])])
         test_transform = transforms.Compose([transforms.Resize(size=(RESIZE, RESIZE)),
                                              transforms.ToTensor(),
@@ -86,30 +109,38 @@ def get_loaders():
         test_transform = None
 
     # create instances of the custom dataset for training and testing
-    train_dataset = CleanDirtyRoadDataset(X=X_train, y=y_train, resize=RESIZE, 
+    train_dataset = CleanDirtyRoadDataset(X=X_train, y=y_train, resize=RESIZE,
                                           data_root=base_img_pth, transform=train_transform)
     test_dataset = CleanDirtyRoadDataset(X=X_test, y=y_test, resize=RESIZE,
                                          data_root=base_img_pth, transform=test_transform)
-    
+
     # pin_memory: speed up the host (cpu) to device (gpu) transfer
     pin = True if torch.cuda.is_available() else False
 
     # create DataLoader instances for training and testing
-    train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, 
+    train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE,
                               shuffle=True, num_workers=os.cpu_count(), pin_memory=pin)
-    test_loader = DataLoader(dataset=test_dataset,batch_size=BATCH_SIZE, 
+    test_loader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE,
                              shuffle=False, num_workers=os.cpu_count(), pin_memory=pin)
-    
+
     return train_loader, test_loader
 
 
 def objective(trial):
+    """
+    Objective function for Optuna hyperparameter optimization.
+
+    Args:
+        trial (optuna.Trial): The trial object for hyperparameter optimization.
+
+    Returns:
+        float: Accuracy obtained with the hyperparameter configuration.
+    """
     # generate the model
     model = finetuning().to(DEVICE)
-    # model = define_model(trial).to(DEVICE)
 
     # generate the optimizer
-    optimizer_name = trial.suggest_categorical("optimizer", ["SGD"]) #"Adam", "RMSprop", 
+    optimizer_name = trial.suggest_categorical("optimizer", ["SGD", "Adam", "RMSprop"])
     lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
     optimizer = getattr(optim, optimizer_name)(model.parameters(), lr=lr)
 
@@ -125,13 +156,13 @@ def objective(trial):
             #     break
             data = data.to(DEVICE)
             target = target.unsqueeze(1).to(DEVICE)
-            logits = model(data)            
+            logits = model(data)
             loss = CRITERION(logits, target)
-            optimizer.zero_grad()            
-            loss.backward()            
+            optimizer.zero_grad()
+            loss.backward()
             optimizer.step()
 
-        # validation of the model.
+        # validation of the model
         predictions = torch.tensor([], device=DEVICE)
         targets = torch.tensor([], device=DEVICE)
         model.eval()
