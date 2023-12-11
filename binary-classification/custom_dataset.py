f27c9ddb577970d9a50f4cb81977d9da6f76ba0a
iamvincenzo <vincenzo.fraello@outlook.it>
2023-12-11 16:29:33 +0100

    update custom_dataset.py

--- binary-classification/custom_dataset.py
+++ binary-classification/custom_dataset.py
@@ -6,19 +6,18 @@ from torch.utils.data import Dataset
 
 class CleanDirtyRoadDataset(Dataset):
     """
-    """
-    def __init__(self, X, y, resize, data_root, transform):
-        """
-        Custom dataset for loading 2D images.
-        https://www.kaggle.com/datasets/faizalkarim/cleandirty-road-classification/
+    Custom dataset for loading 2D images from the Clean and Dirty Road Classification dataset.
+    (https://www.kaggle.com/datasets/faizalkarim/cleandirty-road-classification/)
 
-        Args:
-            X (DataFrame):
-            y (DataFrame):
-            resize (int):
-            data_root (str): Root directory of the dataset.
-            transform (callable): Optional transform to be applied on a sample.
-        """
+    Args:
+        X (pd.DataFrame): DataFrame containing image file names.
+        y (pd.DataFrame): DataFrame containing corresponding class labels.
+        resize (int): Desired size for image resizing.
+        data_root (str or Path): Root directory of the dataset.
+        transform (callable, optional): Optional transform to be applied on a sample.
+            Default is a composition of Resize and ToTensor transformations.
+    """
+    def __init__(self, X, y, resize, data_root, transform=None):
         super(CleanDirtyRoadDataset, self).__init__()
         self.X = X
         self.y = y
@@ -28,27 +27,27 @@ class CleanDirtyRoadDataset(Dataset):
             self.transform = transform
         else:
             self.transform = transforms.Compose([
-                transforms.Resize(size=(resize, resize)), 
+                transforms.Resize(size=(resize, resize)),
                 transforms.ToTensor()
             ])
 
     def __getitem__(self, index):
         """
-        Retrieves a specific item form the dataset.
+        Retrieves a specific item from the dataset.
 
         Args:
             index (int): Index of the item to retrieve.
 
         Returns:
-            tuple: Tuple containing the 2D image data and its corresponding class.
+            tuple: Tuple containing the 2D image data (as a PyTorch tensor)
+            and its corresponding class label (as a float tensor).
         """
         img_name = self.X.iloc[index]
         img_label = self.y.iloc[index]
 
-        img_pth = self.data_root / img_name
+        img_path = self.data_root / img_name
 
-        img = Image.open(img_pth).convert("RGB")
-
+        img = Image.open(img_path).convert("RGB")
         img_tensor = self.transform(img)
         label_tensor = torch.tensor(img_label, dtype=torch.float32)
 
