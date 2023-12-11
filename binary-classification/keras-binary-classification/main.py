654fb89210ed91a70205dabc6635dce8eb9fac04
iamvincenzo <vincenzo.fraello@outlook.it>
2023-12-11 19:38:58 +0100

    update main.py

--- binary-classification/keras-binary-classification/main.py
+++ binary-classification/keras-binary-classification/main.py
@@ -5,27 +5,22 @@ import tensorflow as tf
 from datetime import datetime
 import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
-from keras.regularizers import l2
-from tensorflow.keras import Model
-from tensorflow.keras import layers
 from tensorflow.keras import Sequential
 from sklearn.model_selection import KFold
 from tensorflow.keras.optimizers import Adam
 from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.layers import Flatten
-from tensorflow.python.keras.layers import Dropout
 from keras.callbacks import EarlyStopping, TensorBoard
 from keras.preprocessing.image import ImageDataGenerator
-from tensorflow.python.keras.layers import GlobalAveragePooling2D
 
 
-# set random seed for reproducibility
+# reproducibility
 SEED = 42
 random.seed(SEED)
 np.random.seed(SEED)
 tf.random.set_seed(SEED)
 
-# training parameters
+# parameters
 K_FOLDS = 5
 PATIENCE = 5
 RESIZE = 224
@@ -35,34 +30,54 @@ IMAGE_DIR = "./data/Images"
 METADATA_PATH = "./data/metadata.csv"
 
 
-def get_generators(df, IMAGE_DIR, train_indices, valid_indices):
+def custom_preprocess_input(img):
+    # apply ResNet50 preprocessing
+    img = tf.keras.applications.resnet50.preprocess_input(img)
+    
+    # Add any additional preprocessing steps if needed
+    # For example, you can uncomment and customize the following:
+    # img = tf.image.random_flip_left_right(img)
+    # img = tf.image.random_flip_up_down(img)
+    
+    return img
+
+
+def get_generators(df, image_dir, train_indices, valid_indices):
     """
     Create and return image data generators for training and validation sets.
 
     Args:
-        df (DataFrame): input dataframe that contains image names and labels.
-        IMAGE_DIR (str): image paths.
+        df (pd.DataFrame): input dataframe that contains image names and labels.
+        image_dir (str): image paths.
         train_indices (list): contains indicies of training images.
         valid_indices (list): contains indicies of validation images.
 
     Return:
         training and validation sets.
     """
-    # generate batches of tensor image data with real-time data augmentation.
+    # set up the data generators with the custom preprocessing function
     train_datagen = ImageDataGenerator(
+        preprocessing_function=custom_preprocess_input,
+        # Add other augmentation parameters as needed
         rescale=1.0 / 255,
-        rotation_range=15,
-        width_shift_range=0.1,
-        height_shift_range=0.1,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        fill_mode="nearest",
+        # rotation_range=15,
+        # width_shift_range=0.1,
+        # height_shift_range=0.1,
+        # shear_range=0.2,
+        # zoom_range=0.2,
+        # horizontal_flip=True,
+        # fill_mode="nearest",
     )
 
+    validation_datagen = ImageDataGenerator(
+        preprocessing_function=custom_preprocess_input,
+        rescale=1.0 / 255,
+    )
+
+    # generate batches with the custom preprocessing
     train_generator = train_datagen.flow_from_dataframe(
         dataframe=df.iloc[train_indices],
-        directory=IMAGE_DIR,
+        directory=image_dir,
         x_col="filename",
         y_col="label",
         batch_size=BATCH_SIZE,
@@ -72,14 +87,9 @@ def get_generators(df, IMAGE_DIR, train_indices, valid_indices):
         target_size=(RESIZE, RESIZE),
     )
 
-    # generate batches of tensor image data with real-time data augmentation.
-    valid_datagen = ImageDataGenerator(
-        rescale=1.0 / 255
-    )
-
-    validation_generator = valid_datagen.flow_from_dataframe(
+    validation_generator = validation_datagen.flow_from_dataframe(
         dataframe=df.iloc[valid_indices],
-        directory=IMAGE_DIR,
+        directory=image_dir,
         x_col="filename",
         y_col="label",
         batch_size=BATCH_SIZE,
@@ -92,12 +102,9 @@ def get_generators(df, IMAGE_DIR, train_indices, valid_indices):
     return train_generator, validation_generator
 
 
-def get_model(trainable_layer_count=0):
+def get_model():
     """
     Create and return a binary classification model using a pre-trained ResNet50.
-
-    Args:
-        trainable_layer_count (int): the number of layer to train.
 
     Return:
         A binary classification model using a pre-trained ResNet50.
@@ -115,12 +122,12 @@ def get_model(trainable_layer_count=0):
     binary_model = Sequential()
     binary_model.add(pretrained_model)
     binary_model.add(Flatten())
-    binary_model.add(Dense(1024, activation="relu"))
-    binary_model.add(Dense(2, activation="softmax"))
+    binary_model.add(Dense(1024, activation=None)) # "relu"
+    binary_model.add(Dense(1, activation="sigmoid"))
 
     binary_model.compile(
         optimizer=Adam(learning_rate=0.001),
-        loss="sparse_categorical_crossentropy",
+        loss="binary_crossentropy",
         metrics=["accuracy"],
     )
 
@@ -140,29 +147,24 @@ if __name__ == "__main__":
         print(f"Training on fold {fold + 1}/{K_FOLDS}")
 
         # create data generators for training and validation
-        train_gen, valid_gen = get_generators(
-            df, IMAGE_DIR, train_indices, valid_indices
-        )
+        train_gen, valid_gen = get_generators(df=df, image_dir=IMAGE_DIR, 
+                                              train_indices=train_indices, 
+                                              valid_indices=valid_indices)
 
         # create the binary classification model
-        binary_model = get_model(trainable_layer_count=0)
+        binary_model = get_model()
 
         # add early stopping callback
-        early_stopping = EarlyStopping(
-            monitor="val_loss", patience=PATIENCE, restore_best_weights=True
-        )
+        early_stopping = EarlyStopping(monitor="val_loss", 
+                                       patience=PATIENCE, restore_best_weights=True)
 
         # add TensorBoard callback
         log_dir = "keras_logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
         tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
 
         # train the model
-        history = binary_model.fit(
-            train_gen,
-            validation_data=valid_gen,
-            epochs=NUM_EPOCHS,
-            callbacks=[early_stopping, tensorboard_callback],
-        )
+        history = binary_model.fit(train_gen, validation_data=valid_gen, 
+                                   epochs=NUM_EPOCHS, callbacks=[early_stopping, tensorboard_callback])
 
         # evaluate the model on the validation set
         test_loss, test_accuracy = binary_model.evaluate(valid_gen)
