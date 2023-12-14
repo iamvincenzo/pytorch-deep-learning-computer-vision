42e1fa02f6bf7e338e40060e5ff1678aa114aebf
iamvincenzo <vincenzo.fraello@outlook.it>
2023-12-15 00:24:49 +0100

    update main.py: major update clean dataset

--- object-detection/main.py
+++ object-detection/main.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 import torch
 import random
 import numpy as np
@@ -15,14 +16,17 @@ from dataset import PlantDocDataset
 # parameters
 SEED = 42
 EPOCHS = 200
-RESIZE = 128
+RESIZE = 224
 PATIENCE = 5
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 
 TEST_IMGS_PATH = Path("./data/TEST")
 TRAIN_IMGS_PATH = Path("./data/TRAIN")
 METADATA_TEST_PATH = Path("./data/test_labels.csv")
 METADATA_TRAIN_PATH = Path("./data/train_labels.csv")
+
+FIXED_TRAIN_FILE_PATH = "./data/train_labels_fixed.csv"
+FIXED_TEST_FILE_PATH = "./data/test_labels_fixed.csv"
 
 # reproducibility
 random.seed(SEED)
@@ -31,17 +35,60 @@ torch.manual_seed(SEED)
 torch.cuda.manual_seed(SEED)
 
 
+def clean_dataframe(df, imgs_root_path, save_path):
+    """
+    Offline dataframe preprocessing.
+    """
+    # remove images that don't exist from training data
+    df = df[[filename in os.listdir(imgs_root_path) for filename in df["filename"]]]
+    
+    # fix items: there are some values of width (w) and height (h) 
+    # that are either set to 0 or do not match those of the actual images
+    for idx, row in df.iterrows():
+        filename = row["filename"]
+        img = cv2.imread(str(imgs_root_path / filename))
+    
+        if img is not None:
+            height, width, _ = img.shape
+            if row["height"] != height or row["width"] != width:
+                df.at[idx, "height"] = height
+                df.at[idx, "width"] = width
+
+    condition = df["width"] < df["xmax"]
+    df = df.drop(df[condition].index)
+
+    condition = df["height"] < df["ymax"]
+    df = df.drop(df[condition].index)
+
+    # export the DataFrame to a CSV file
+    df.to_csv(save_path, index=False)
+
+    return df
+
+
 if __name__ == "__main__":
-    # read and filter training metadata (remove images that don't exist)
-    train_df = pd.read_csv(filepath_or_buffer=METADATA_TRAIN_PATH)
-    filtered_train_df = train_df[[os.path.isfile(TRAIN_IMGS_PATH / filename) for filename in train_df["filename"]]]
+    if not os.path.exists(FIXED_TRAIN_FILE_PATH):
+        # read train metadata
+        train_df = pd.read_csv(filepath_or_buffer=METADATA_TRAIN_PATH)
+        # clean train dataframe
+        train_df = clean_dataframe(df=train_df, imgs_root_path=TRAIN_IMGS_PATH, save_path=FIXED_TRAIN_FILE_PATH)
+    else:
+        # read train metadata
+        train_df = pd.read_csv(filepath_or_buffer=FIXED_TRAIN_FILE_PATH)
 
-    # read and filter testing metadata (remove images that don't exist)
-    test_df = pd.read_csv(filepath_or_buffer=METADATA_TEST_PATH)
-    filtered_test_df = test_df[[os.path.isfile(TEST_IMGS_PATH / filename) for filename in test_df["filename"]]]
-
+    if not os.path.exists(FIXED_TEST_FILE_PATH):
+        # read test metadata
+        test_df = pd.read_csv(filepath_or_buffer=METADATA_TEST_PATH)
+        # clean test dataframe
+        test_df = clean_dataframe(df=test_df, imgs_root_path=TEST_IMGS_PATH,  save_path=FIXED_TEST_FILE_PATH)
+    else:
+        # read train metadata
+        test_df = pd.read_csv(filepath_or_buffer=FIXED_TEST_FILE_PATH)
+    
     # prepare class labels and mappings
-    classes_list = sorted(filtered_train_df["class"].unique())
+    # reload not cleaned train_df because it can contain classes that are in test_df but not in cleaned train_df 
+    train_df_classes_original = pd.read_csv(filepath_or_buffer=METADATA_TRAIN_PATH)
+    classes_list = sorted(train_df_classes_original["class"].unique())
     classes_list.insert(0, "__background__")
     train_classes = {cls: idx for idx, cls in enumerate(classes_list, start=0)}
     train_classes_inverse = {idx: cls for idx, cls in enumerate(classes_list, start=0)}
@@ -51,9 +98,9 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # create training and test datasets
-    train_dataset = PlantDocDataset(df=filtered_train_df, transform=None, resize=224, 
+    train_dataset = PlantDocDataset(df=train_df, data_aug=True, resize_h=RESIZE, resize_w=RESIZE,
                                     root=TRAIN_IMGS_PATH, classes=train_classes, classes_inverse=train_classes_inverse)    
-    test_dataset = PlantDocDataset(df=filtered_test_df, transform=None, resize=224, 
+    test_dataset = PlantDocDataset(df=test_df, data_aug=False, resize_h=RESIZE, resize_w=RESIZE,
                                    root=TEST_IMGS_PATH, classes=train_classes, classes_inverse=train_classes_inverse)
     
     # determine if GPU pinning is possible
@@ -64,7 +111,7 @@ if __name__ == "__main__":
                               num_workers=os.cpu_count(), pin_memory=pin, collate_fn=collate_fn)
     test_loader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False, 
                              num_workers=os.cpu_count(), pin_memory=pin, collate_fn=collate_fn)
-
+    
     # initialize the model and move to the computation device
     model = create_model(num_classes=NUM_CLASSES)
     model = model.to(device)
